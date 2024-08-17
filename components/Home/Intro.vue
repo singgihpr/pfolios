c5b3774e6118c48e3af9f83abd936b97f4397b74
singgihpr <singgih.hans@gmail.com>
2024-08-17 22:33:15 +0700

    Feat: copywrite homepage

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="space-y-6">
-    <NuxtImg src="/avatar.png" alt="Fayaz Ahmed"
+    <NuxtImg src="/singgih.jpg" alt="Singgih Aji"
       class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
       sizes="48px sm:64px" placeholder format="webp" />
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
       Hello!
     </h1>
     <p class="text-gray-900 dark:text-gray-400">
-      I'm Fayaz, I work as a software, product engineer and designer from
-      Bengaluru, India. I specialize in building web applications and sites
-      using Javascript, React, Vue &amp; Node. I've procrastinated building this
-      website for years but finally it's here, I've carved out my own little
-      nook on the internet to share my silly experiments, nifty projects, and
-      thoughts (mostly about tech and design).
+      My name is Singgih Aji, I'm a Fullstack Web Developer from
+      Jakarta, Indonesia.<br /> I specialize in building web applications and sites
+      using Laravel, Vue &amp; Nuxt. Also, I'm a father to a daughter and husband to beautiful wife.
     </p>
     <p class="text-gray-900 dark:text-gray-400">
-      By day, I'm a Fullstack Developer at
-      <a href="https://headshotpro.com" target="_blank" class="underline">Headshotpro</a>, and
-      by night (and weekends), I'm busy tinkering with some random tool or app
-      that I am building.
+      By day, I'm a Fullstack Web Developer doubles as Project Manager at
+      <a href="https://salamsolusi.com" target="_blank" class="underline">salamsolusi</a>, and
+      by night (and weekends), I'm busy side hustling for my family.
     </p>
   </div>
 </template>
 
 <script setup>
 useSeoMeta({
-  title: "Fayaz Ahmed",
+  title: "Singgih Aji",
   description:
-    "I'm Fayaz, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
+    "I'm Singgih, I work as a Fullstack Web Developer from Jakarta, Indonesia. I specialize in building web applications and sites using Laravel, Vue & Nuxt.",
 });
 </script>
